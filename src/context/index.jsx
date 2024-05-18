65c8505a71eced410e328caf236e759bbf2be835
Udeme98 <Darealudeme1@gmail.com>
2024-05-18 11:24:29 +0100

    each recipe card setup

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,24 +4,36 @@ export const GlobalContext = createContext(null);
 
 const GlobalState = ({ children }) => {
   const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipe, setRecipe] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setLoading(true);
     try {
       const res = await fetch(
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
       );
       const data = await res.json();
 
-      console.log(data);
+      if (data?.data?.recipes) {
+        setRecipe(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
+
+      //   console.log(data);
     } catch (error) {
       console.log(error);
+      setLoading(false);
+      setSearchParam("");
     }
   };
 
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSubmit }}
+      value={{ searchParam, loading, recipe, setSearchParam, handleSubmit }}
     >
       {children}
     </GlobalContext.Provider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,25 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context";
+import RecipeItem from "../components/RecipeItem";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { loading, recipe } = useContext(GlobalContext);
+  //   console.log(recipe);
+
+  if (loading) return <div>Loading... Please Wait!</div>;
+
+  return (
+    <div className="container mx-auto py-8 flex flex-wrap justify-center gap-10">
+      {recipe && recipe.length > 0 ? (
+        recipe.map((item) => <RecipeItem item={item} key={item.id} />)
+      ) : (
+        <div>
+          <p className="text-xl text-center text-black font-extrabold lg:text-3xl">
+            Nothing to Show. Please A Recipe!
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 export default Home;
